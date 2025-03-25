f755cbd5ca9a26c39fa57c53e238a06c57e2d691
AlyssaSantos002 <aj.csantos1602@gmail.com>
2025-03-25 13:47:12 -0400

    init commit

---
+++ src/test/java/com/humber/quizverse/QuizVerseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.humber.quizverse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizVerseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
